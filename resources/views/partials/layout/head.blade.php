af26640c985d808a75b7060a153ad54b12c30053
Kareem Dabbeet <kareemda1@gmail.com>
2020-01-08 14:01:49 +0200

    adding metronic

---
+++ resources/views/partials/layout/head.blade.php
@@ -0,0 +1,20 @@
+<!-- begin::Head -->
+<head>
+    <meta charset="utf-8" />
+    <title>Basita | @yield('title', 'Dashboard')</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!--begin::Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700">
+    <!--end::Fonts -->
+    <!--begin::Page Vendors Styles(used by this page) -->
+    @yield('page-level-css')
+    <!--end::Page Vendors Styles -->
+    <!--begin::Global Theme Styles(used by all pages) -->
+    {{-- <link href="/assets/plugins/global/plugins.bundle.css" rel="stylesheet" type="text/css" /> --}}
+    <link href="/css/style.min.css" rel="stylesheet" type="text/css" />
+    <!--end::Global Theme Styles -->
+    <!--begin::Layout Skins(used by all pages) -->
+    <!--end::Layout Skins -->
+    <link rel="shortcut icon" href="/assets/media/logos/favicon.ico" />
+</head>
+<!-- end::Head -->
---
+++ resources/views/partials/master.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+@include('partials.layout.head')
+<!-- begin::Body -->
+
+<body>
+    @yield('content')
+</body>
+
+</html>
