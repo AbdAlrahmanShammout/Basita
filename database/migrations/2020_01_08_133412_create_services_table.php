4c17920ff1186d73832c18f816875871b8a7f5f3
Abd Shammout <abdshammout.97@gmail.com>
2020-01-08 17:26:20 +0200

    add migrations.

---
+++ database/migrations/2020_01_08_133412_create_services_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateServicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('services', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('type_service_id');
+            $table->bigInteger('user_id');
+            $table->string('mobile_number_work', 10);
+            $table->string('phone_number_work');
+            $table->text('description');
+            $table->time('available_time');
+            $table->text('work_address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('services');
+    }
+}
