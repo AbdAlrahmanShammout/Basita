14fb9a5640c6b85ef0dcdf6492f43d78e7fb063d
waseemBirkdar <57323000+waseemBirkdar@users.noreply.github.com>
2020-01-08 18:04:11 +0200

    add other model

--- app/Favourite.php
+++ app/Favourite.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Favourite extends Model
 {
-    //
+    protected $table = "favourites";
+
+    protected $fillable = [
+        'id',  'user_id' ,'service_id','start'
+    ];
+
+    protected $primaryKey = "id";
+
+    public $timestamps = true;
 }
+
+
--- app/Region.php
+++ app/Region.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Region extends Model
 {
-    //
-}
+    protected $table = "regions";
+
+    protected $fillable = [
+        'id',  'name'
+    ];
+
+    protected $primaryKey = "id";
+
+    public $timestamps = true;
+};
