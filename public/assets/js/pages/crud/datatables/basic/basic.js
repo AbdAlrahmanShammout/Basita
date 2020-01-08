2c0f425f44484886d942da21d05460bfcd6ac7ae
Kareem Dabbeet <kareemda1@gmail.com>
2020-01-09 01:54:06 +0200

    clean tree

--- public/assets/js/pages/crud/datatables/basic/basic.js
+++ public/assets/js/pages/crud/datatables/basic/basic.js
@@ -40,62 +40,62 @@ var KTDatatablesBasicBasic = (function() {
 				// 		</a>`;
                 //     }
                 // },
-                {
-                    targets: 2,
-                    render: function(data, type, full, meta) {
-                        var status = {
-                            1: { title: "Pending", class: "kt-badge--brand" },
-                            2: {
-                                title: "Delivered",
-                                class: " kt-badge--danger"
-                            },
-                            3: {
-                                title: "Canceled",
-                                class: " kt-badge--primary"
-                            },
-                            4: {
-                                title: "Success",
-                                class: " kt-badge--success"
-                            },
-                            5: { title: "Info", class: " kt-badge--info" },
-                            6: { title: "Danger", class: " kt-badge--danger" },
-                            7: { title: "Warning", class: " kt-badge--warning" }
-                        };
-                        if (typeof status[data] === "undefined") {
-                            return data;
-                        }
-                        return (
-                            '<span class="kt-badge ' +
-                            status[data].class +
-                            ' kt-badge--inline kt-badge--pill">' +
-                            status[data].title +
-                            "</span>"
-                        );
-                    }
-                },
-                {
-                    targets: 3,
-                    render: function(data, type, full, meta) {
-                        var status = {
-                            1: { title: "Online", state: "danger" },
-                            2: { title: "Retail", state: "primary" },
-                            3: { title: "Direct", state: "success" }
-                        };
-                        if (typeof status[data] === "undefined") {
-                            return data;
-                        }
-                        return (
-                            '<span class="kt-badge kt-badge--' +
-                            status[data].state +
-                            ' kt-badge--dot"></span>&nbsp;' +
-                            '<span class="kt-font-bold kt-font-' +
-                            status[data].state +
-                            '">' +
-                            status[data].title +
-                            "</span>"
-                        );
-                    }
-                }
+                // {
+                //     targets: 2,
+                //     render: function(data, type, full, meta) {
+                //         var status = {
+                //             1: { title: "Pending", class: "kt-badge--brand" },
+                //             2: {
+                //                 title: "Delivered",
+                //                 class: " kt-badge--danger"
+                //             },
+                //             3: {
+                //                 title: "Canceled",
+                //                 class: " kt-badge--primary"
+                //             },
+                //             4: {
+                //                 title: "Success",
+                //                 class: " kt-badge--success"
+                //             },
+                //             5: { title: "Info", class: " kt-badge--info" },
+                //             6: { title: "Danger", class: " kt-badge--danger" },
+                //             7: { title: "Warning", class: " kt-badge--warning" }
+                //         };
+                //         if (typeof status[data] === "undefined") {
+                //             return data;
+                //         }
+                //         return (
+                //             '<span class="kt-badge ' +
+                //             status[data].class +
+                //             ' kt-badge--inline kt-badge--pill">' +
+                //             status[data].title +
+                //             "</span>"
+                //         );
+                //     }
+                // },
+                // {
+                //     targets: 3,
+                //     render: function(data, type, full, meta) {
+                //         var status = {
+                //             1: { title: "Online", state: "danger" },
+                //             2: { title: "Retail", state: "primary" },
+                //             3: { title: "Direct", state: "success" }
+                //         };
+                //         if (typeof status[data] === "undefined") {
+                //             return data;
+                //         }
+                //         return (
+                //             '<span class="kt-badge kt-badge--' +
+                //             status[data].state +
+                //             ' kt-badge--dot"></span>&nbsp;' +
+                //             '<span class="kt-font-bold kt-font-' +
+                //             status[data].state +
+                //             '">' +
+                //             status[data].title +
+                //             "</span>"
+                //         );
+                //     }
+                // }
             ]
         });
 
