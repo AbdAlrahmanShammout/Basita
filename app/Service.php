9d19f7bd4a719d4edeb60faf88e8430c49bfa77b
Abd Shammout <abdshammout.97@gmail.com>
2020-01-08 17:51:15 +0200

    add Service Model.

--- app/Service.php
+++ app/Service.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Service extends Model
 {
-    //
+    protected $table = "services";
+
+    protected $fillable = [
+        'id',  'type_service_id',  'user_id',  'mobile_number_work'
+        ,  'phone_number_work',  'description'
+        ,  'available_time',  'work_address',
+    ];
+
+    protected $primaryKey = "id";
+
+    public $timestamps = true;
+
 }
