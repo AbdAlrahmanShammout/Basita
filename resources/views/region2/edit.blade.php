76eee990cce9e29d66e316494debd954102b14fe
Kareem Dabbeet <kareemda1@gmail.com>
2020-01-09 13:22:08 +0200

    bug fix

--- resources/views/region2/edit.blade.php
+++ resources/views/region2/edit.blade.php
@@ -23,7 +23,7 @@
             <div class="alert alert-light alert-elevate fade show" role="alert">
                 <div class="alert-icon"><i class="flaticon-warning kt-font-brand"></i></div>
                 <div class="alert-text">
-                    Please Note that you cant add new service type name that is used before <br>
+                    Please Note that you cant add new region name that is used before <br>
                     please enter unique name
                 </div>
             </div>
@@ -35,22 +35,33 @@
         <div class="kt-portlet__head">
             <div class="kt-portlet__head-label">
                 <h3 class="kt-portlet__head-title">
-                    Add New Service Type
+                    Add New region
                 </h3>
             </div>
         </div>
 
         <!--begin::Form-->
-        <form class="kt-form kt-form--label-right" method="post" action="{{ route('region.update',$serviceType->id)}}">
+        <form class="kt-form kt-form--label-right" method="post" action="{{ route('region.update',$region->id)}}">
             @method('patch')
 
             @csrf
             <div class="kt-portlet__body">
                 <div class="form-group row">
-                    <label class="col-form-label col-lg-3 col-sm-12">service type name</label>
+                    <label class="col-form-label col-lg-3 col-sm-12">region name</label>
                     <div class="col-lg-6 col-md-9 col-sm-12">
-                        <input type="text" name="name" class="form-control" value="{{$serviceType->name}}"
-                            placeholder="please enter your service type name">
+                        <input type="text" name="name" class="form-control" value="{{$region->name}}"
+                            placeholder="please enter your region name">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-form-label col-lg-3 col-sm-12">region city</label>
+                    <div class="col-lg-6 col-md-9 col-sm-12">
+                        <select class="form-control selectpicker" id="selectUser" name="city_id"
+                            title="Please select city" required focus>
+                            @foreach($cities as $city)
+                            <option value="{{$city->id}}">{{ $city->name }}</option>
+                            @endforeach
+                        </select>
                     </div>
                 </div>
             </div>
